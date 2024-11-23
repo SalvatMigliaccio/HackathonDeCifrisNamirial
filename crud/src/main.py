ee2720fb3823cd009c34143a0d33f226be611c73
wolfic709 <wolfictor@gmail.com>
2024-11-23 01:58:07 +0100

    developed endpoints

--- crud/src/main.py
+++ crud/src/main.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from chain_utils import ChainUtils as cu
+from models import SignRequest
+from ring_signature import Ring as rs
+
 
 app = FastAPI()
 
 
-# sign document
 @app.post("/sign")
-def upload_sign(
-    sign: list[int],
-    entity_seed: str,
-    dossier_address: str,
-):
-    memos = [cu.parse_memo(hex(s)) for s in sign]
-    cu.send_transaction(entity_seed, dossier_address, memos)
+def upload_sign(sign_request: SignRequest):
+    g = get_group_by_id(sign_request.group)
+    ring = rs(g)
+    verification = ring.verify_message(sign_request.document_uri, sign_request.sign)
+    if not verification:
+        raise HTTPException(status_code=401)
+
+    memos = [cu.parse_memo(hex(s)) for s in sign_request.sign]
+    tx = cu.send_transaction(
+        sign_request.entity_seed, sign_request.dossier_address, memos
+    )
+    return tx.to_dict.get("hash")
 
 
-@app.get("/dossier")
+@app.get("/dossier/{dossier_address}", response_model=list[str])
 def read_dossier(dossier_address):
-    return cu.get_txs(dossier_address)
+    try:
+        return cu.get_txs(dossier_address)
+    except Exception as e:
+        raise e
 
 
 if __name__ == "__main__":
